24e9a44be0547be76cf1eb00cf5e429b7fe0aa7e
unknown <ssanym211@gmail.com>
2023-02-24 20:01:11 +0300

    at new  array and while

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("массив : ");
+string[] array={"car","Monday","dogs","2","344"};
+Console.WriteLine(array.Length);
+int i=0;
+while(i<array.Length)
+{
+Console.WriteLine(array[i].Length);
+i++; 
+Console.WriteLine(i);
+}
