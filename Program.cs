26d590dcaf2c99ac8ed3190a3908b390d941ce01
unknown <ssanym211@gmail.com>
2023-02-26 12:53:57 +0300

    new information about file

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("массив : ");
+string[] array={"car","Monday","dogs","2","344"};
+int i=0;
+while(i<array.Length)
+{
+int index=array[i].Length;
+if(index<=3) Console.WriteLine(array[i]);
+i++; 
+}
